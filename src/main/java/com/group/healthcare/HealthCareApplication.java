f02ee743e6c43c269c713edb7bcfe7bf3584d055
LakshanWeerasinghe <lak98weerasinghe@gmail.com>
2020-03-24 01:26:50 +0530

    initial commit

---
+++ src/main/java/com/group/healthcare/HealthCareApplication.java
@@ -0,0 +1,13 @@
+package com.group.healthcare;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class HealthCareApplication {
+	
+	public static void main(String args[]) {
+		SpringApplication.run(HealthCareApplication.class, args);
+	}
+
+}
