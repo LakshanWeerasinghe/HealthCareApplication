d36a03eb741c2a2d0ecb1b7f639f82107de4a6bd
LakshanWeerasinghe <lak98weerasinghe@gmail.com>
2020-03-24 20:42:31 +0530

    new entities added

---
+++ src/main/java/com/group/healthcare/entity/DoctorSpecialization.java
@@ -0,0 +1,9 @@
+package com.group.healthcare.entity;
+
+public class DoctorSpecialization {
+	
+	private int id;
+	
+	private String specillizationFiled;
+
+}
---
+++ src/main/java/com/group/healthcare/entity/DoctorTimeAllocation.java
@@ -0,0 +1,14 @@
+package com.group.healthcare.entity;
+
+import java.time.LocalDateTime;
+
+public class DoctorTimeAllocation {
+	
+	private int id;
+	
+	private String room;
+	
+	private LocalDateTime time;
+	
+
+}
